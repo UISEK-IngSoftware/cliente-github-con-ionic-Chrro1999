cdb760094fbb0661d0cac87e8da25b0f58bbaf4b
laboratorio.fs <christian.bolanos@uisek.edu.ec>
2025-12-03 11:07:24 -0500

    Lab7 movil

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,15 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonItem,
+  IonLabel,
+  IonThumbnail,
+} from '@ionic/react';
+
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -16,7 +26,36 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonList>
+           <IonItem>
+            <IonThumbnail slot="start">
+              <img alt="Silhouette of mountains" src="https://1000marcas.net/wp-content/uploads/2020/01/Android-Logo-2014-1.png" />
+            </IonThumbnail>
+            <IonLabel>Item</IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonThumbnail slot="start">
+              <img alt="Silhouette of mountains" src="https://1000marcas.net/wp-content/uploads/2020/01/Android-Logo-2014-1.png" />
+            </IonThumbnail>
+            <IonLabel>Item</IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonThumbnail slot="start">
+              <img alt="Silhouette of mountains" src="https://1000marcas.net/wp-content/uploads/2020/01/Android-Logo-2014-1.png" />
+            </IonThumbnail>
+            <IonLabel>Item</IonLabel>
+          </IonItem>
+
+          <IonItem lines="none">
+            <IonThumbnail slot="start">
+              <img alt="Silhouette of mountains" src="https://1000marcas.net/wp-content/uploads/2020/01/Android-Logo-2014-1.png" />
+            </IonThumbnail>
+            <IonLabel>Item</IonLabel>
+          </IonItem>
+        </IonList>
+        
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,30 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg } from '@ionic/react';
 import './Tab3.css';
+
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil del usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil del usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <IonCard>
+          <IonCardHeader>
+            <IonImg alt="Silhouette of mountains" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRC-KflR5lYPFVaQgXfTTETXCg82fRoXCqJEN5a8X2xTqLVy-tPbReqiJFuN-bIa9ewPhug-BCp8nV4qWjWPPGh5JwoG9ouhhG-ygWTYZ3mg&s=10" />
+            <IonCardTitle>Christian Bolaños</IonCardTitle>
+            <IonCardSubtitle>Chrro1999</IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>Soy un desarrollador de software apasionado a las tegnologias</IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
